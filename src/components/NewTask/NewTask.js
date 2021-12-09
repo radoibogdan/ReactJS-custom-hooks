dc08e6faa1610ab1874a267cf212770513472806
YBogdan RADOI <radoi.office@gmail.com>
2021-12-09 22:34:03 +0100

    Using custom hook in get and post

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,41 +1,28 @@
-import { useState } from 'react';
+import useHttp from '../../hooks/use-http';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const {isLoading, error, sendRequest : sendTaskRequest} = useHttp();
+  
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+    props.onAddTask(createdTask);
+  }
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    sendTaskRequest({
+      url : 'https://react-movie-http-95492-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
+      method : 'POST',
+      header : {
+        'Content-Type': 'application/json',
+      },
+      body : { text: taskText }
+      // bind - preconfigure 1st arg of createTask to be taskText
+      // taskData will be appended after taskText when called
+    }, createTask.bind(null, taskText));
   };
 
   return (
